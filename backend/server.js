6285e625a06741358c112be4c1918511af69d102
prosenjeetshil <prosenjeetshil@gmail.com>
2025-06-18 16:53:54 +0530

    feat: configure nodemon & setup API routes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,19 @@ const express = require("express");
 const app = express()
 
 app.get("/api/notes", (req, res)=>{
-    res.status(200).send("You have 4 notes");
+    res.status(200).send("You have 5 notes");
+});
+
+app.post("/api/notes", (req, res)=>{
+    res.status(201).json({ message:"Note created successfully!"});
+});
+
+app.put("/api/notes/:id", (req, res)=>{
+    res.status(200).json({ message:"Note updated successfully!"});
+});
+
+app.delete("/api/notes/:id", (req, res)=>{
+    res.status(200).json({ message:"Note deleted successfully!"});
 });
 
 app.listen(5001, ()=>{
