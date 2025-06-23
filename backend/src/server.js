9aa81d7623188b4a45e5bfe90aa283cd90a98746
prosenjeetshil <prosenjeetshil@gmail.com>
2025-06-23 11:05:13 +0530

    feat: mongoDB database setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const notesRoutes = require("./routes/notesRoutes");
+const connectDB = require("./config/db");
 
 const app = express()
+const PORT = process.env.PORT || 5001
+connectDB();
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, ()=>{
-    console.log("Server started on PORT: 5001");
+app.listen(PORT, ()=>{
+    console.log("Server started on PORT:", PORT);
 });
+
