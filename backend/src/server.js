8f33268d70ea6e710f8d4bc25fc31f8351ea9ccd
prosenjeetshil <prosenjeetshil@gmail.com>
2025-06-26 12:39:53 +0530

    feat: add CORS support, implement note fetching, and enhance UI components

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const notesRoutes = require("./routes/notesRoutes");
 const connectDB = require("./config/db");
 const rateLimiter = require("./middleware/rateLimiter");
+const cors = require("cors");
 
 const app = express()
 const PORT = process.env.PORT || 5001
@@ -9,8 +10,17 @@ const PORT = process.env.PORT || 5001
 
 
 // middleware
+
+//cors should be used before other middlewares
+// to allow cross-origin requests from the frontend
+
+app.use(cors({
+    origin: "http://localhost:5173",
+}));
+
 app.use(express.json());
 app.use(rateLimiter);
+
 
 app.use("/api/notes", notesRoutes);
 
